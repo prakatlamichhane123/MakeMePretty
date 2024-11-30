e0028afb697844659b974c45a376f16b2e822cd1
Prakat Lamichhane <prakatlamichhane@gmail.com>
2024-11-30 15:12:14 +0545

    Changing title and locating ankurs bug and removing unnessary style files

--- mmp-frontend/src/Home.jsx
+++ mmp-frontend/src/Home.jsx
@@ -1,5 +1,14 @@
 function Home(){
+    return(
+    <>
     <h1>This is home</h1>
+    <h1>This is home</h1>
+    <h1>This is home</h1>
+    <h1>This is home</h1>
+    <h1>This is home</h1>
+    <h1>This is home</h1>
+    <h1>This is home</h1>
+    </>)
 }
 
 export default Home;
